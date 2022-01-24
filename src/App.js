1cf4d56f5f1053a4f4b2c69b9e8c217d0f6d57ab
ruile <ruilegan10@gmail.com>
2022-01-24 22:29:59 +0800

    initial login and view blogs functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,85 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import login from './services/login'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+
+    // should send username and password to backend
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  // const blogForm = () => {
+  //   <form onSubmit={addBlog}>
+  //     <input>
+  //       value={newBlog}
+  //       onChange={handleBlogChange}
+  //     </input>
+  //     <button type='submit'>save</button>
+  //   </form>
+  // }
+
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {loginForm()}
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -19,6 +89,7 @@ const App = () => {
       )}
     </div>
   )
+
 }
 
 export default App
